2b9ce758a255e4a2cbd66cbb674b585df50ae82c
Danial Dehghan <daniyaldehghan2021@gmail.com>
2025-01-04 15:00:08 +0330

    Add unit tests for main.fc contract functionality
    - Created tests/main.spec.ts to test contract logic in main.fc.
    - Verified the counter increment functionality upon receiving a specific operation code.
    - Validated the contract correctly updates and retrieves the most recent sender's address.
    - Utilized the sandbox environment for contract deployment and interaction.
    - Ensured transaction success and data integrity using assertions.

---
+++ tests/main.spec.ts
@@ -0,0 +1,43 @@
+import { Cell, toNano } from "ton-core";
+import { hex } from "../build/main.compiled.json";
+import { Blockchain } from "@ton-community/sandbox";
+import { MainContract } from "../wrappers/MainContract";
+import "@ton-community/test-utils";
+
+describe("main.fc contract tests", () => {
+  it("should successfully increase counter in contract and get the proper most recent sender addr", async () => {
+    const blockchain = await Blockchain.create();
+    const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+
+    const initAddress = await blockchain.treasury("initAddress");
+
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig(
+        {
+          number: 0,
+          address: initAddress.address,
+        },
+        codeCell
+      )
+    );
+
+    const senderWallet = await blockchain.treasury("sender");
+
+    const sentMessageResult = await myContract.sendIncrementComment(
+      senderWallet.getSender(),
+      toNano("0.05"),
+      1
+    );
+
+    expect(sentMessageResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
+    });
+
+    const data = await myContract.getData();
+
+    expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual;
+  });
+});
