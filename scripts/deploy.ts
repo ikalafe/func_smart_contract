5ccce8b01c26533a6b3a3f0429450680ea1f2981
Danial Dehghan <daniyaldehghan2021@gmail.com>
2025-01-04 10:02:41 +0330

    Enhance deploy script with dynamic environment-based configuration
    - Integrated dotenv for environment variable handling.
    - Adjusted deploy link to support both testnet and mainnet based on TESTNET environment variable.
    - Added clear logs indicating deployment to testnet or mainnet.
    - Reduced initial deployment amount to 0.0001 TON for efficiency.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -10,6 +10,9 @@ import { hex } from "../build/main.compiled.json";
 import qs from "qs";
 import qrcode from "qrcode-terminal";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 async function deployScript() {
   console.log(
     "================================================================="
@@ -36,17 +39,17 @@ async function deployScript() {
   console.log(
     `The address of the contract is following: ${address.toString()}`
   );
-  console.log(`Please Scan the QRCode below to to deploy the contract: `);
+  console.log(`Please Scan the QRCode below to to deploy the contract to ${process.env.TESTNET ? "testnet" : "mainnet"}:`);
 
   let link =
-    `https://test.tonhub.com/transfer/` +
+    `https://${process.env.TESTNET ? "test." : ""}.tonhub.com/transfer/` +
     address.toString({
-      testOnly: true,
+      testOnly: process.env.TESTNET ? true : false,
     }) +
     "?" +
     qs.stringify({
       text: "Deploy contract",
-      amount: BigInt(0.05 * 1_000_000_000).toString(10),
+      amount: BigInt(0.0001 * 1_000_000_000).toString(10),
       init: stateInitCell.toBoc({ idx: false }).toString("base64"),
     });
   qrcode.generate(link, { small: true }, (code) => {
