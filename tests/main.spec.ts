b5b20f37baab51ac2e8233777d94eea74e620361
Danial Dehghan <daniyaldehghan2021@gmail.com>
2025-01-05 16:58:26 +0330

    Expand Test Coverage for main.fc Contract with Comprehensive Scenarios
    - **Test Enhancements:**
      - **Counter Increment Tests:**
        - Ensure counter increment functionality works and updates recent sender.
    
      - **Deposit Functionality:**
        - Verify successful deposit of funds to the contract.
        - Ensure funds are returned if no operation code is specified.
    
      - **Withdrawal Functionality:**
        - Validate successful withdrawal by the contract owner.
        - Test unauthorized withdrawal attempts by non-owners with appropriate error handling.
        - Confirm failed withdrawal when contract balance is insufficient.
    
    - **New Test Cases Added:**
      - successfully deposits funds
      - should return deposit funds as no command is sent
      - successfully withdraws funds on behalf of owner
      - fails to withdraw funds on behalf of non-owner
      - fails to withdraw funds because lack of balance
    
    - **Code Refinements:**
      - Enhanced test readability and modularity.
      - Utilized mock sender wallets and treasury integration for simulation.

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -51,6 +51,97 @@ describe("main.fc contract tests", () => {
     const data = await myContract.getData();
 
     expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
-    expect(data.number).toEqual;
+    expect(data.number).toEqual(1);
+  });
+
+  it("successfully deposits funds", async () => {
+    const senderWallet = await blockchain.treasury("sender");
+
+    const depositMsgResult = await myContract.sendDeposit(
+      senderWallet.getSender(),
+      toNano("5")
+    );
+
+    expect(depositMsgResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: true,
+    });
+
+    const balanceReq = await myContract.getCurrentBalance();
+
+    expect(balanceReq.balance).toBeGreaterThan(toNano("4.99"));
+  });
+
+  it("should return deposit funds as no command is sent", async () => {
+    const senderWallet = await blockchain.treasury("sender");
+
+    const depositMsgResult = await myContract.sendNoCodeDeposit(
+      senderWallet.getSender(),
+      toNano("5")
+    );
+
+    expect(depositMsgResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: false,
+    });
+
+    const balanceReq = await myContract.getCurrentBalance();
+
+    expect(balanceReq.balance).toEqual(0);
+  });
+
+  it("successfully withdraws funds on behalf of owner", async () => {
+    const senderWallet = await blockchain.treasury("sender");
+
+    await myContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+
+    const withdrawalReqResult = await myContract.sendWithdrawalReq(
+      ownerWalletAddress.getSender(),
+      toNano("0.5"),
+      toNano(1)
+    );
+
+    expect(withdrawalReqResult.transactions).toHaveTransaction({
+      from: myContract.address,
+      to: ownerWalletAddress.address,
+      success: true,
+      value: toNano(1),
+    });
+  });
+
+  it("fails to withdraw funds on behalf of non-owner", async () => {
+    const senderWallet = await blockchain.treasury("sender");
+
+    await myContract.sendDeposit(senderWallet.getSender(), toNano("5"));
+
+    const withdrawalReqResult = await myContract.sendWithdrawalReq(
+      senderWallet.getSender(),
+      toNano("0.5"),
+      toNano("1")
+    );
+
+    expect(withdrawalReqResult.transactions).toHaveTransaction({
+      from: senderWallet.address,
+      to: myContract.address,
+      success: false,
+      exitCode: 103,
+    });
+  });
+
+  it("fails to withdraw funds because lack of balance", async () => {
+    const withdrawalReqResult = await myContract.sendWithdrawalReq(
+      ownerWalletAddress.getSender(),
+      toNano("0.5"),
+      toNano("1")
+    );
+
+    expect(withdrawalReqResult.transactions).toHaveTransaction({
+      from: ownerWalletAddress.address,
+      to: myContract.address,
+      success: false,
+      exitCode: 104,
+    });
   });
 });
